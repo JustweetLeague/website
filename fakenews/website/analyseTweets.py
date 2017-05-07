2676af5f1a65ebc946242da752424b67a6195799
Anna <annayamsh@gmail.com>
2017-05-07 16:09:55 +0100

    Updated database

--- fakenews/website/analyseTweets.py
+++ fakenews/website/analyseTweets.py
@@ -6,6 +6,6 @@ return json of format { "score": ?, "problems": [?, ?] }
 """
 
 def analyse(tweet):
-    response = requests.get('http://46.101.95.25:8000/analyse?id=' + tweet['id_str'],timeout=5)
+    response = requests.get('http://46.101.95.25:8000/analyse?id=' + tweet['id_str'])
     body = response.content
     return json.loads(body)
